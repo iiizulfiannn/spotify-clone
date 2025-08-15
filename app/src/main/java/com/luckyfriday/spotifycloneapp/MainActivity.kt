42922b5951b0d62504898d8b3eadcc9b295265fb
zul <iiizulfiannn@gmail.com>
2025-08-15 13:03:03 +0700

    feat: implement onclicked on main screen

--- app/src/main/java/com/luckyfriday/spotifycloneapp/MainActivity.kt
+++ app/src/main/java/com/luckyfriday/spotifycloneapp/MainActivity.kt
@@ -1,24 +1,38 @@
 package com.luckyfriday.spotifycloneapp
 
+import android.content.BroadcastReceiver
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.isVisible
+import androidx.media3.common.Player
 import androidx.recyclerview.widget.ItemTouchHelper
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.luckyfriday.spotifycloneapp.adapter.MusicAdapter
 import com.luckyfriday.spotifycloneapp.databinding.ActivityMainBinding
 import com.luckyfriday.spotifycloneapp.listener.ItemMoveCallback
+import com.luckyfriday.spotifycloneapp.listener.MusicActionServiceListener
 import com.luckyfriday.spotifycloneapp.listener.MusicListener
 import com.luckyfriday.spotifycloneapp.model.MusicModel
+import com.luckyfriday.spotifycloneapp.notification.NotificationReceiver
 
 class MainActivity : AppCompatActivity(), MusicListener {
     private lateinit var listener: MusicListener
     private lateinit var mainBinding: ActivityMainBinding
     private var listMusic = ArrayList<MusicModel>()
     private val musicAdapter: MusicAdapter by lazy { MusicAdapter(listMusic, listener) }
+    private val notificationListener: MusicActionServiceListener by lazy {
+        NotificationReceiver()
+    }
+    private var repeatMode: Int = Player.REPEAT_MODE_OFF
+    private var isShuffleEnable: Boolean = false
+    private var isPlaying = false
+    private var currentPosition = -1
+    private var progress = -1f
+    private var duration = ""
+    private var durationTotal = ""
+    private var selectedMusicPlayed: MusicModel? = null
+    private lateinit var dataFromService: BroadcastReceiver
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -43,6 +57,61 @@ class MainActivity : AppCompatActivity(), MusicListener {
     }
 
     override fun onClicked(music: MusicModel) {
-        TODO("Not yet implemented")
+        currentPosition = listMusic.indexOf(music)
+        selectedMusicPlayed = listMusic[currentPosition]
+        isPlaying = true
+
+        // restart music
+        restartMusic()
+
+        // play button
+        playButton()
+    }
+
+    private fun playButton() {
+        // show hide now playing
+        showHideLinePlaying()
+
+        // show handle player
+        showMusicPlayer()
+
+        if (currentPosition == -1) return
+
+        if (isPlaying) {
+            mainBinding.layoutButtonHandle.btnPlay.setImageResource(R.drawable.ic_paused)
+            mainBinding.musicPlayingNowFullScreen.layoutBtnDetailScreen.btnPlay.setImageResource(R.drawable.ic_paused)
+        } else {
+            mainBinding.layoutButtonHandle.btnPlay.setImageResource(R.drawable.ic_play)
+            mainBinding.musicPlayingNowFullScreen.layoutBtnDetailScreen.btnPlay.setImageResource(R.drawable.ic_play)
+        }
+
+        musicAdapter.setSelectedMusic(selectedMusicPlayed)
+    }
+
+    private fun showMusicPlayer() {
+        mainBinding.layoutButtonHandle.apply {
+            root.isVisible = true
+        }
+    }
+
+    private fun showHideLinePlaying() {
+        // now playing
+        mainBinding.layoutMusicNowPlaying.apply {
+            root.isVisible = true
+            tvTitleNowPlaying.text = selectedMusicPlayed?.title
+            tvDescriptionNowPlaying.text = selectedMusicPlayed?.description
+            ivNowPlaying.loadImage(this@MainActivity, selectedMusicPlayed?.imageCover.orEmpty())
+        }
+
+        // fullscreen
+        mainBinding.musicPlayingNowFullScreen.apply {
+            tvTitleSong.text = selectedMusicPlayed?.title
+            tvDescriptionSong.text = selectedMusicPlayed?.description
+            ivAlbumCover.loadImage(this@MainActivity, selectedMusicPlayed?.imageCover.orEmpty())
+        }
+    }
+
+    private fun restartMusic() {
+        notificationListener.onRestart(this, currentPosition)
     }
 }
